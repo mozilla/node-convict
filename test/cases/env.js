2d934f7a3270523ef19b3da0a2275df32a02db07
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-25 15:06:35 -0700

    failing test of setting params via env

---
+++ test/cases/env.js
@@ -0,0 +1,15 @@
+exports.conf = {
+  ip: {
+    format: 'string = "127.0.0.1"',
+    env: "IP_ADDRESS"
+  },
+  port: {
+    format: 'number = 0',
+    env: "PORT"
+  }
+};
+
+exports.env = {
+  IP_ADDRESS: '10.0.1.101',
+  PORT: 8080
+};

d5e26a762e23933e6755bfcc3c5d570290661a2d
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-03-18 14:21:49 -0400

    Document and test that convict throws on reuse of an env var

--- test/schema-tests.js
+++ test/schema-tests.js
@@ -23,6 +23,13 @@ describe('convict schema file', function() {
     conf = convict(path.join(__dirname, 'cases/schema-built-in-formats.json'));
   });
 
+  it('must throw when parsing a specification that reuses an env variable', function() {
+    (function() { convict({
+      foo: {default: 'a', env: 'BAZ'},
+      bar: {default: 'a', env: 'BAZ'}
+    })}).must.throw();
+  })
+
   describe('after being parsed', function() {
 
     beforeEach(function() {

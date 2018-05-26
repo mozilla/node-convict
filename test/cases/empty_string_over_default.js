94ab37aaf221cea86fe4ce353bad5d3010b1ce69
Dan Allen <dan.j.allen@gmail.com>
2018-05-26 01:39:59 -0600

    resolves #230 allow argument value to be falsy (#246)
    - value of falsy argument should override default value

--- test/cases/empty_string_over_default.js
+++ test/cases/empty_string_over_default.js
@@ -6,8 +6,15 @@ exports.conf = {
     format: String,
     env: 'PREFIX'
   },
+  suffix: {
+    default: '.js',
+    format: String,
+    arg: 'suffix'
+  },
 };
 
 exports.env = {
   PREFIX: '',
 };
+
+exports.argv = ['node', 'index.js', '--suffix=']
